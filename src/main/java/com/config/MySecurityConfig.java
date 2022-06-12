8ea4f7b3c6ea8a6ee86b136aebd9d90bcc7483dc
Asen Sanjula <asensanjula@gmail.com>
2022-06-12 20:56:19 +0530

    feat: add inMemory Authentication with NoOp password encoder

--- src/main/java/com/config/MySecurityConfig.java
+++ src/main/java/com/config/MySecurityConfig.java
@@ -1,13 +1,32 @@
 package com.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     //this class
 
 
+    @Override
+    protected void configure( AuthenticationManagerBuilder auth ) throws Exception {
+
+        auth.inMemoryAuthentication()
+            .withUser( "asen" )
+            .password( "123" )
+            .roles( "admin" );
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+
+        return  NoOpPasswordEncoder.getInstance();
+    }
 }
