0fda3bd911f3d7c32ca0b7517a7c366ce1ed4a8e
Asen Sanjula <asensanjula@gmail.com>
2022-06-12 19:48:44 +0530

    feat: add spring security chain filter

---
+++ src/main/java/com/config/MySecurityConfig.java
@@ -0,0 +1,13 @@
+package com.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //this class
+
+
+}
